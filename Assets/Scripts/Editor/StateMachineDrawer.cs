e752643fe8948ceb26a324bf5efeedfda4db1560
Vast <Vastlee@gmail.com>
2017-11-04 18:22:52 -0800

    Added StateMachine/State/And an Example Scene/MonoBehaviour

---
+++ Assets/Scripts/Editor/StateMachineDrawer.cs
@@ -0,0 +1,68 @@
+﻿/* Description: Quick Drawer to interface with the state machine.
+ * Displays Current State, State Status, and provides drop down for
+ * switching states.
+ * Brogrammer: Vast
+ */
+
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(StateMachine))]
+[CanEditMultipleObjects]
+public class StateMachineDrawer : PropertyDrawer {
+    private StateMachine stateMachine;
+    private GUIContent[] states;
+    private int currentSelection = 0;
+    private int previousSelection = 0;
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        if(this.stateMachine == null) {
+            this.stateMachine = this.fieldInfo.GetValue(property.serializedObject.targetObject) as StateMachine;
+            if(this.stateMachine != null) {
+                this.stateMachine.OnStateChange += this.OnStateChange;
+                this.stateMachine.OnStateAdd += this.OnStateAdd;
+                this.stateMachine.OnStateRem += this.OnStateRem;
+                UpdateStatesList();
+            } else {
+                return;
+            }
+        }
+
+        EditorGUI.BeginProperty(position, label, property);
+        EditorGUI.PrefixLabel(position, label);
+        position.x += EditorGUIUtility.labelWidth;
+        position.width -= EditorGUIUtility.labelWidth;
+        this.currentSelection = EditorGUI.Popup(position, this.currentSelection, this.states);
+        EditorGUI.EndProperty();
+
+        if(this.currentSelection != this.previousSelection) {
+            this.stateMachine.ChangeState(this.stateMachine.States[this.currentSelection]);
+            this.previousSelection = this.currentSelection;
+        }
+    }
+
+    /// <summary>
+    /// Returns the States Names as a string array
+    /// </summary>
+    /// <returns></returns>
+    private void UpdateStatesList() {
+        int stateCount = this.stateMachine.States.Count;
+        this.states = new GUIContent[stateCount];
+        for(int i = 0; i < stateCount; i++) {
+            this.states[i] = new GUIContent(this.stateMachine.States[i].Name);
+        }
+    }
+
+    private void OnStateChange(State changeToState) {
+        this.currentSelection = this.stateMachine.States.IndexOf(this.stateMachine.ActiveState);
+    }
+
+    private void OnStateAdd(State stateAdded) {
+        UpdateStatesList();
+    }
+
+
+    private void OnStateRem(State stateRemoved) {
+        UpdateStatesList();
+    }
+}
---
+++ Assets/Scripts/StateMachineExample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachineExample : MonoBehaviour {
+    [SerializeField] private StateMachine entityState;
+
+    private State idleState = new State("Idle");
+    private State runningState = new State("Running");
+    private State jumpingState = new State("Jumping");
+
+    public void Awake() {
+        entityState = StateMachine.NewStateMachine();
+        //new State("NONE", entityState); // Example of adding a state to a State Machine upon creation
+        entityState.AddStates(idleState, runningState, jumpingState); // Example of adding already created States.
+        entityState.AddState("Flying"); // Example of adding a new State with just a name.
+    }
+
+    public void Update() {
+        entityState.UpdateActiveState();
+    }
+
+    public void FixedUpdate() {
+        entityState.FixedUpdateActiveState();
+    }
+}
